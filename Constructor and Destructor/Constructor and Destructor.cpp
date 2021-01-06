474de449168c8be4f642c0ab9e441793c4c16ab7
Jayram Patil <jayram1234patil@gmail.com>
2021-01-07 00:43:29 +0530

    Update Constructor and Destructor.cpp

--- Constructor and Destructor/Constructor and Destructor.cpp
+++ Constructor and Destructor/Constructor and Destructor.cpp
@@ -1,48 +1,48 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// Demonstrate Program for Constructor and Destructor Types and Call Sequence.
+/// Program for Types Of Constructor and Destructor.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include<iostream>
 #include<conio.h>
 using namespace std;
 
-class Const_Dest             // Constructor and Destructor Name of Class
+class Const_Dest             // Constructor and Destructor Have Same Name As Class
 {
-                private:            // Private Access Specifier
-                            int x,y; // Data Member or Characteristics of Class
+      private:            // Private Access Specifier
+              int x,y; // Data Member or Characteristics of Class
 
-                public:             // Public Access Specifier
+      public:             // Public Access Specifier
 
-                           Const_Dest()  // Default Constructor
-                           {
-                                    cout<<"\n Inside Default Constructor...!!!"<<endl;
-                                    this-> x = 0;
-                                    this-> y = 0;
-                           }
+             Const_Dest()  // Default Constructor
+             {
+                      cout<<"\n Inside Default Constructor...!!!"<<endl;
+                      this-> x = 0;
+                      this-> y = 0;
+             }
 
-                           Const_Dest(int iNo1,int iNo2) // Parameterized Constructor
-                           {
-                                    cout<<"\n Inside Parameterized Constructor...!!!"<<endl;
-                                    this->x = iNo1;
-                                    this->y = iNo2;
-                           }
+             Const_Dest(int iNo1,int iNo2) // Parameterized Constructor
+             {
+                      cout<<"\n Inside Parameterized Constructor...!!!"<<endl;
+                      this->x = iNo1;
+                      this->y = iNo2;
+             }
 
-                           Const_Dest(Const_Dest &ref) // Copy Constructor
-                           {
-                                    cout<<"\n Inside Copy Constructor...!!!"<<endl;
-                                    this->x = ref.x;
-                                    this->y=ref.y;
-                           }
+             Const_Dest(Const_Dest &ref) // Copy Constructor
+             {
+                      cout<<"\n Inside Copy Constructor...!!!"<<endl;
+                      this->x = ref.x;
+                      this->y=ref.y;
+             }
 
-                           void Display() // Member Function or Behavior of Class
-                           {
-                                    cout<<"\n x = "<<x<<"\n y = "<<y<<endl;
-                           }
+             void Display() // Member Function or Behavior of Class
+             {
+                      cout<<"\n x = "<<x<<"\n y = "<<y<<endl;
+             }
 
-                           ~Const_Dest() // Destructor (Destructor call will Flow Bottom to Top Approach)
-                           {
-                                     cout<<"\n Inside Destructor....!!! x = "<<x<<endl;
-                           }
+             ~Const_Dest() // Destructor (Destructor call will Flow Bottom to Top Approach)
+             {
+                       cout<<"\n Inside Destructor....!!! x = "<<x<<endl;
+             }
 };
 
 int main()
@@ -51,24 +51,23 @@ int main()
 
 
 
-      Const_Dest cobj1; // Default Constructor Object
-      cobj1.Display();
+      Const_Dest obj1; // Default Constructor Object
+      obj1.Display();
 
       cout<<"\n Please Enter Two Parameters to Pass for Parameterized Constructor =>"<<endl;
       cin>>a>>b;
 
-      Const_Dest cobj2(a,b); // Parameterized Constructor Object
-      cobj2.Display();
+      Const_Dest obj2(a,b); // Parameterized Constructor Object
+      obj2.Display();
 
-      Const_Dest cobj3(cobj1); // Copy Constructor Object
-      cobj3.Display();
+      Const_Dest obj3(obj1); // Copy Constructor Object
+      obj3.Display();
 
-      Const_Dest cobj4(cobj2); // Copy Constructor Object Made Recopy Constructor Because it was giving error of Redeclaration
-      cobj4.Display();
+      Const_Dest obj4(obj2);
+      obj4.Display();
 
       getch();
 
       return 0;
 
-      /// Note :- Destructor Will Class Default By Compiler
 }
